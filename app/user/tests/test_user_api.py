37fd56cd232a20121fbfa38caefc01567b8e4df4
obarowskij <153957508+obarowskij@users.noreply.github.com>
2024-04-28 21:56:57 +0200

    Update test_user_api.py

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,4 +1,4 @@
-#Tests for user API.
+# Tests for user API.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -9,10 +9,12 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 
+
 def create_user(**params):
     get_user_model().objects.create_user(**params)
 
 class PublicUserAPITest(TestCase):
+
     
     def setUp(self):
         self.client = APIClient()
@@ -41,12 +43,13 @@ class PublicUserAPITest(TestCase):
 
     def test_password_too_short(self):
         payload = {
-            'email':'test1@example.com',
-            'password':'123',
+            'email': 'test1@example.com',
+            'password': '123',
             'name': 'test'
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         with self.assertRaises(get_user_model().DoesNotExist) as context:
             get_user_model().objects.get(email=payload['email'])
-        self.assertEqual(str(context.exception), "User matching query does not exist.")
+        self.assertEqual(str(context.exception),
+                         "User matching query does not exist.")
