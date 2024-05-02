bce5d5f50a886a15ad62f7f275b4524d53e3906b
obarowskij <obarowskijakub13@gmail.com>
2024-05-02 12:36:30 +0200

    a

--- app/user/urls.py
+++ app/user/urls.py
@@ -5,7 +5,7 @@ from user import views
 app_name = "user"
 
 urlpatterns = [
-    path("create/",views.CreateUserView.as_view(), name="create"),
+    path("create/", views.CreateUserView.as_view(), name="create"),
     path("token/", views.TokenView.as_view(), name="token"),
     path("me/", views.RetrieveUser.as_view(), name="me"),
 ]
